9d613a45706248950c7adfd5f95400cf9cfaaa55
karina-ranadive <karinaranadive@gmail.com>
2023-04-04 19:58:54 -0400

    Added unit tests for Counter.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,38 @@
 // import necessary react testing library helpers here
-// import the Counter component here
+import React from 'react'
+import { getByTestId, render, screen, fireEvent} from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom/extend-expect'
+
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+    render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const message = screen.getByRole('heading');
+  expect(message).toHaveTextContent('Counter');
+
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const val = screen.queryByTestId('count');
+  expect(val).toHaveTextContent(0);
+
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  test('clicking + increments the count', () => {
+  fireEvent.click(screen.getByText('+'));
+  const val = screen.queryByTestId('count');
+  expect(val).toHaveTextContent(1);
+  
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText('-'));
+  const val = screen.queryByTestId('count');
+  expect(val).toHaveTextContent(-1);
+
 });
